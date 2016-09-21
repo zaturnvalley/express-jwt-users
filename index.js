887dafa226eedca22d6aa509c573f25a82818537
zaturnvalley <zacharycmason@gmail.com>
2016-09-21 14:15:07 -0700

    added auth post route

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var User = require('./models/user');
 var app = express();
+
+var secret = 'bestkeptsecretever999';
 
 mongoose.connect('mongodb://localhost:27017/myauthenticatedusers');
 
@@ -10,6 +15,17 @@ app.use('/api/users', require('./controllers/users'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/api/auth', function(req, res) {
+  User.findOne({email: req.body.email}, function(err, user) {
+    if(err || !user) return res.send({message: 'User Not Found'});
+    user.authenticated(req.body.password, function(err, result) {
+      if(err || !user) return res.send({message: 'User Not Authenticated'});
+      var token = jwt.sign(user, secret);
+      res.send({user: user, token: token});
+      });
+    }); 
 });
 
 app.listen(3000);
